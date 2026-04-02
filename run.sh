0837edacc621767e241ab6923b8139be3a950db6
zhangwei <zhangwei@chitu.ai>
2026-04-02 20:23:19 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./script/srun_multi_node.sh 1 1 \
     --master_port=22525 \
-    -m chitu \
+    test/single_req_test.py \
     serve.port=21111 \
     infer.tp_size=1 \
     infer.cache_type=paged \
@@ -8,7 +8,7 @@
     models.ckpt_dir=/data/nfs/LLaDA2.1-mini \
     infer.use_cuda_graph=True \
     infer.attn_type=dllm \
-    infer.max_reqs=16 \
+    infer.max_reqs=1 \
     infer.max_seq_len=1200 \
     request.max_new_tokens=1024 \
     infer.use_cuda_graph=True >& chitu_run.log
